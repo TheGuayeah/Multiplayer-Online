166ab660cd288607c11fa7845f1f80f679ae930d
TheGuayeah <37774329+TheGuayeah@users.noreply.github.com>
2020-11-29 15:37:00 +0100

    Steam friends invite

--- Assets/_Completed-Assets/Scripts/SteamLobby.cs
+++ Assets/_Completed-Assets/Scripts/SteamLobby.cs
@@ -6,15 +6,58 @@ using Steamworks;
 
 public class SteamLobby : MonoBehaviour
 {
+    public static CSteamID LobbyID { get; private set; }
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEnter;
+
+    private const string HostAddresKey = "HostAddress";
     private NetworkManager networkManager;
 
     void Start()
     {
         networkManager = GetComponent<NetworkManager>();
+
+        if (!SteamManager.Initialized) return;
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEnter);
     }
 
     public void HostLobby()
     {
         SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t calllback)
+    {
+        if (calllback.m_eResult != EResult.k_EResultOK) return;
+
+        LobbyID = new CSteamID(calllback.m_ulSteamIDLobby);
+        PlayerPrefs.SetString("NickName", LobbyID.ToString());
+
+        networkManager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(
+            new CSteamID(calllback.m_ulSteamIDLobby), 
+            HostAddresKey, SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEnter(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active) return;
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby), HostAddresKey);
+
+        networkManager.networkAddress = hostAddress;
+        networkManager.StartClient();
     }
 }
