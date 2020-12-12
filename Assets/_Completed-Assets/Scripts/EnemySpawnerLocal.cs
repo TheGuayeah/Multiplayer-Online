f65f7d2acf68db616ffa99d3b99641dd056b43dc
Jonatan <jonatanfmembrives@gmail.com>
2020-12-12 19:58:31 +0900

    local started

---
+++ Assets/_Completed-Assets/Scripts/EnemySpawnerLocal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class EnemySpawnerLocal : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int numberOfEnemies;
+
+    public void Start()
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            var spawnPosition = new Vector3(
+            Random.Range(-8.0f, 8.0f),
+            0.0f,
+            Random.Range(-8.0f, 8.0f));
+            var spawnRotation = Quaternion.Euler(
+            0.0f,
+            Random.Range(0, 180),
+            0.0f);
+            Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+        }
+    }
+}
