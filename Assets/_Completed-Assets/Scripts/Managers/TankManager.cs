ced3ffcd2630c9c33e9bb930f5a2b43eda25e571
Miquel Bellet <miquel.bellet@gmail.com>
2020-12-19 10:31:51 +0100

    local game

--- Assets/_Completed-Assets/Scripts/Managers/TankManager.cs
+++ Assets/_Completed-Assets/Scripts/Managers/TankManager.cs
@@ -21,40 +21,64 @@ namespace Complete
 
         private TankMovement m_Movement;                        // Reference to tank's movement script, used to disable and enable control
         private TankShooting m_Shooting;                        // Reference to tank's shooting script, used to disable and enable control
+        private TankMovementLocal m_MovementLocal;                        // Reference to tank's movement script, used to disable and enable control
+        private TankShootingLocal m_ShootingLocal;                        // Reference to tank's shooting script, used to disable and enable control
         private GameObject m_CanvasGameObject;                  // Used to disable the world space UI during the Starting and Ending phases of each round
 
 
         public void Setup ()
         {
-            // Get references to the components
-            m_Movement = m_Instance.GetComponent<TankMovement> ();
-            m_Shooting = m_Instance.GetComponent<TankShooting> ();
-            m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas> ().gameObject;
+            try
+            {
+                // Get references to the components
+                m_Movement = m_Instance.GetComponent<TankMovement>();
+                m_Shooting = m_Instance.GetComponent<TankShooting>();
+                m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
 
-            // Set the player numbers to be consistent across the scripts
-            m_Movement.m_PlayerNumber = m_PlayerNumber;
-            m_Shooting.m_PlayerNumber = m_PlayerNumber;
+                // Set the player numbers to be consistent across the scripts
+                m_Movement.m_PlayerNumber = m_PlayerNumber;
+                m_Shooting.m_PlayerNumber = m_PlayerNumber;
+            }
+            catch (Exception)
+            {
+                // Get references to the components
+                m_MovementLocal = m_Instance.GetComponent<TankMovementLocal>();
+                m_ShootingLocal = m_Instance.GetComponent<TankShootingLocal>();
+                m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
+
+                // Set the player numbers to be consistent across the scripts
+                m_MovementLocal.m_PlayerNumber = m_PlayerNumber;
+                m_ShootingLocal.m_PlayerNumber = m_PlayerNumber;
+            }
 
             // Create a string using the correct color that says 'PLAYER 1' etc based on the tank's color and the player's number
             m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
 
             // Get all of the renderers of the tank
-            //MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer> ();
+            MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer> ();
 
             // Go through all the renderers...
-            /*for (int i = 0; i < renderers.Length; i++)
+            for (int i = 0; i < renderers.Length; i++)
             {
                 // ... set their material color to the color specific to this tank
                 renderers[i].material.color = m_PlayerColor;
-            }*/
+            }
         }
 
 
         // Used during the phases of the game where the player shouldn't be able to control their tank
         public void DisableControl ()
         {
-            m_Movement.enabled = false;
-            m_Shooting.enabled = false;
+            try
+            {
+                m_Movement.enabled = false;
+                m_Shooting.enabled = false;
+            }
+            catch (Exception)
+            {
+                m_MovementLocal.enabled = false;
+                m_ShootingLocal.enabled = false;
+            }
 
             m_CanvasGameObject.SetActive (false);
         }
@@ -63,9 +87,16 @@ namespace Complete
         // Used during the phases of the game where the player should be able to control their tank
         public void EnableControl ()
         {
-            m_Movement.enabled = true;
-            m_Shooting.enabled = true;
-
+            try
+            {
+                m_Movement.enabled = true;
+                m_Shooting.enabled = true;
+            }
+            catch (Exception)
+            {
+                m_MovementLocal.enabled = true;
+                m_ShootingLocal.enabled = true;
+            }
             m_CanvasGameObject.SetActive (true);
         }
 
