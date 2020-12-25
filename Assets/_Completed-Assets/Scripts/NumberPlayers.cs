a9d7d69075801e8644b99260f6a89e2fef420d65
jorge ch <jorgecalle.92@gmail.com>
2020-12-25 23:31:01 +0100

    Lógica de juegoi

---
+++ Assets/_Completed-Assets/Scripts/NumberPlayers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NumberPlayers : NetworkBehaviour
+{
+    [SyncVar]
+    public int currentNumberPlayers;
+
+    private void Update()
+    {
+        currentNumberPlayers = GameObject.FindGameObjectsWithTag("Player").Length;
+    }
+}
