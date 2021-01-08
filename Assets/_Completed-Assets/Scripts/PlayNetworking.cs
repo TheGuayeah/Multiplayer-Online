595bfa9d3dc2efd3c939051770079ed10885a6a6
Miquel Bellet <miquel.bellet@gmail.com>
2021-01-08 15:00:29 +0100

    play btn max 2 players

--- Assets/_Completed-Assets/Scripts/PlayNetworking.cs
+++ Assets/_Completed-Assets/Scripts/PlayNetworking.cs
@@ -8,6 +8,7 @@ namespace Complete
     public class PlayNetworking : NetworkBehaviour
     {
         public GameObject playBtn;
+        public GameObject teamsCanvas;
         public bool activeTeams = true;
 
         [SyncVar(hook = nameof(StartGameNetwork))]
@@ -15,6 +16,9 @@ namespace Complete
 
         [SerializeField]
         private GameManager gameManager;
+
+        private int team1Players;
+        private int team2Players;
 
         void Start()
         {
@@ -32,14 +36,41 @@ namespace Complete
 
         public void StartGame()
         {
-            gameStart = true;
-            GameObject[] tanksInGame = GameObject.FindGameObjectsWithTag("Player");
-            foreach (var tank in tanksInGame)
+            checkNumPlayers();
+            if (team1Players < 3 && team2Players < 3)
             {
-                tank.GetComponent<TankMovement>().enabled = true;
-                tank.GetComponent<TankShooting>().enabled = true;
+                gameStart = true;
+                teamsCanvas.SetActive(false);
+                GameObject[] tanksInGame = GameObject.FindGameObjectsWithTag("Player");
+                foreach (var tank in tanksInGame)
+                {
+                    tank.GetComponent<TankMovement>().enabled = true;
+                    tank.GetComponent<TankShooting>().enabled = true;
+                }
+                tanksInGame[0].GetComponent<InfoPlayer>().RpcStartGame(true);
             }
-            tanksInGame[0].GetComponent<InfoPlayer>().RpcStartGame(true);
+        }
+
+        private void checkNumPlayers()
+        {
+            // Start the count of tanks left at zero
+            int team1TanksLeft = 0;
+            int team2TanksLeft = 0;
+
+            // Go through all the tanks...
+            for (int i = 0; i < gameManager.m_Tanks.Length; i++)
+            {
+                // ... and if they are active, increment the counter
+                if (gameManager.m_Tanks[i].m_Instance.activeSelf && gameManager.m_Tanks[i].m_Instance.GetComponent<InfoPlayer>() != null)
+                {
+                    if (gameManager.m_Tanks[i].m_Instance.GetComponent<InfoPlayer>().team1)
+                        team1TanksLeft++;
+                    else
+                        team2TanksLeft++;
+                }
+            }
+            team1Players = team1TanksLeft;
+            team2Players = team2TanksLeft;
         }
     }
 }
